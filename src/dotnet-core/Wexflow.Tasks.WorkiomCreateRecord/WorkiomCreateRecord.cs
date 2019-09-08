4ed4d00a51e33a3789110ff6d3338904b86ae2fb
aelassas <akram.elassas@gmail.com>
2019-09-08 16:11:00 +0100

    Updated the task WorkiomCreateRecord

--- src/dotnet-core/Wexflow.Tasks.WorkiomCreateRecord/WorkiomCreateRecord.cs
+++ src/dotnet-core/Wexflow.Tasks.WorkiomCreateRecord/WorkiomCreateRecord.cs
@@ -1,6 +1,9 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
 using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading;
 using System.Xml.Linq;
 using Wexflow.Core;
@@ -9,11 +12,12 @@ namespace Wexflow.Tasks.WorkiomCreateRecord
 {
     public class WorkiomCreateRecord : Task
     {
-
+        public string CreateRecordUrl { get; }
         public string MappingFile { get; }
 
         public WorkiomCreateRecord(XElement xe, Workflow wf) : base(xe, wf)
         {
+            CreateRecordUrl = GetSetting("createRecordUrl");
             MappingFile = GetSetting("mappingFile");
         }
 
@@ -25,10 +29,29 @@ namespace Wexflow.Tasks.WorkiomCreateRecord
 
             try
             {
-                // TODO parse JSON
+                // Parse JSON
                 string json = File.ReadAllText(MappingFile);
-                JObject o = JObject.Parse(json);
-                // TODO REST call
+                var o = JObject.Parse(json);
+                var auth = (string)o.SelectToken("Autorization");
+                var listId = (string)o.SelectToken("listId");
+                var payload = o.SelectToken("Payload").ToString();
+
+                // REST call
+                var url = CreateRecordUrl + listId;
+                var responseTask = Post(url, auth, payload);
+                responseTask.Wait();
+                var response = responseTask.Result;
+                var responseSuccess = (bool)JObject.Parse(response).SelectToken("success");
+                
+                if (responseSuccess)
+                {
+                    Info("Record created.");
+                }
+                else
+                {
+                    ErrorFormat("An error occured while creating the record: {0}", response);
+                    success = false;
+                }
 
             }
             catch (ThreadAbortException)
@@ -51,5 +74,22 @@ namespace Wexflow.Tasks.WorkiomCreateRecord
             Info("Task finished.");
             return new TaskStatus(status);
         }
+
+        private async System.Threading.Tasks.Task<string> Post(string url, string auth, string json)
+        {
+            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", auth);
+                var httpResponse = await httpClient.PostAsync(url, httpContent);
+                if (httpResponse.Content != null)
+                {
+                    var responseContent = await httpResponse.Content.ReadAsStringAsync();
+                    return responseContent;
+                }
+            }
+            return string.Empty;
+        }
+
     }
 }
