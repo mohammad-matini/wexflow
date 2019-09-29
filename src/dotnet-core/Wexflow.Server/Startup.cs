464a35b8c1bd607d8ebf64fdd2555f8326940442
aelassas <akram.elassas@gmail.com>
2019-09-29 16:01:02 +0100

    Downgrade to .net core 2.2

--- src/dotnet-core/Wexflow.Server/Startup.cs
+++ src/dotnet-core/Wexflow.Server/Startup.cs
@@ -1,34 +1,29 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Nancy.Owin;
 
 namespace Wexflow.Server
 {
     public class Startup
     {
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        private readonly IConfiguration _config;
+
+        public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json")
                 .SetBasePath(env.ContentRootPath);
 
-            var config = builder.Build();
-
-            var appConfig = new AppConfiguration();
-            ConfigurationBinder.Bind(config, appConfig);
-
-            app.UseOwin(x => x.UseNancy(opt => opt.Bootstrapper = new Bootstrapper(appConfig)));
+            _config = builder.Build();
         }
 
-        public void ConfigureServices(IServiceCollection services)
+        public void Configure(IApplicationBuilder app)
         {
-            services.Configure<KestrelServerOptions>(options =>
-            {
-                options.AllowSynchronousIO = true;
-            });
+            var appConfig = new AppConfiguration();
+            ConfigurationBinder.Bind(_config, appConfig);
+
+            app.UseOwin(x => x.UseNancy(opt => opt.Bootstrapper = new Bootstrapper(appConfig)));
         }
     }
 }
